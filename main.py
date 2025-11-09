1965777d9b107ae2d6f221dad8425b5d9f76bed6
toya <96932308+toyaAoi@users.noreply.github.com>
2025-11-09 20:20:07 +0530

    done

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-from stats import get_num_words, get_char_counts
+from stats import get_num_words, get_char_counts, get_chars_sorted
+import sys
 
-text = get_num_words("./books/frankenstein.txt")
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+book_path = sys.argv[1]
+text = get_num_words(book_path)
 num_words = len(text.split())
+char_counts = get_char_counts(text)
+print("============ BOOKBOT ============")
+print(f"Analyzing book found at {num_words}...")
+print("----------- Word Count ----------")
 print(f"Found {num_words} total words")
-print(get_char_counts(text))
+print("--------- Character Count -------")
+for char in get_chars_sorted(char_counts):
+    print(f"{char["char"]}: {char["num"]}")
--- stats.py
+++ stats.py
@@ -8,9 +8,21 @@ def get_char_counts(text):
     char_count = {}
 
     for char in text:
+        char = char.lower()
         if char in char_count:
             char_count[char] += 1
         else:
             char_count[char] = 1
 
     return char_count
+
+
+def get_chars_sorted(dict):
+    char_list = []
+
+    for char in dict:
+        if char.isalpha():
+            char_list.append({"char": char, "num": dict[char]})
+
+    char_list.sort(key=lambda x: x["num"], reverse=True)
+    return char_list
